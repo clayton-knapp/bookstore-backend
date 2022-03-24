bd82e7d6b11e0847201ec7a1ed788977ab930397
Clayton Knapp <csjknapp@gmail.com>
2022-03-24 14:33:33 -0700

    changed id names

--- sql/setup.sql
+++ sql/setup.sql
@@ -9,7 +9,7 @@ DROP TABLE IF EXISTS reviewer CASCADE;
 DROP TABLE IF EXISTS author_book CASCADE;
 
 CREATE TABLE publisher(
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+  publisher_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
   name TEXT NOT NULL,
   city TEXT,
   state TEXT,
@@ -18,70 +18,70 @@ CREATE TABLE publisher(
 
 
 CREATE TABLE book(
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+  book_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
   title TEXT NOT NULL,
-  publisher_id BIGINT NOT NULL, FOREIGN KEY (publisher_id) REFERENCES publisher(id),
+  publisher_id BIGINT NOT NULL, FOREIGN KEY (publisher_id) REFERENCES publisher(publisher_id),
   released INT NOT NULL
 );
 
 
-  CREATE TABLE author(
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
-    name TEXT NOT NULL,
-    dob DATE,
-    pob TEXT 
-  );
+CREATE TABLE author(
+  author_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+  name TEXT NOT NULL,
+  dob DATE,
+  pob TEXT 
+);
 
   CREATE TABLE reviewer(
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+  reviewer_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
   name TEXT NOT NULL,
   company TEXT NOT NULL
 );
 
   CREATE TABLE review(
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+    review_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
     rating INT NOT NULL,
-    reviewer_id BIGINT NOT NULL, FOREIGN KEY (reviewer_id) REFERENCES reviewer(id),
+    reviewer_id BIGINT NOT NULL, FOREIGN KEY (reviewer_id) REFERENCES reviewer(reviewer_id),
     review TEXT NOT NULL,
-    book_id BIGINT NOT NULL, FOREIGN KEY (book_id) REFERENCES book(id) 
+    book_id BIGINT NOT NULL, FOREIGN KEY (book_id) REFERENCES book(book_id) 
     );
 
   CREATE TABLE author_book(
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
-    author_id BIGINT REFERENCES author(id),
-    book_id BIGINT REFERENCES book(id)
+    author_book_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+    author_id BIGINT REFERENCES author(author_id),
+    book_id BIGINT REFERENCES book(book_id)
 );
 
-INSERT INTO 
-  publisher (name, city, state, country)
+INSERT INTO publisher 
+  (name, city, state, country)
 VALUES
   ('Random House', 'New York City', 'NY', 'USA'),
   ('Penguin', 'Los Angeles', 'CA', 'USA');
 
-INSERT INTO
-  book (title, publisher_id, released)
+INSERT INTO book 
+  (title, publisher_id, released)
 VALUES
   ('Harry Potter', '1', 1999),
   ('Lord of the Rings', '2', 1965),
   ('Harry Potter 3', '1', 2003);
 
-INSERT INTO
-  author (name, dob, pob)
-  VALUES
+INSERT INTO author 
+  (name, dob, pob)
+VALUES
   ('JK Rowling', '1965-07-31', 'Yate, England'),
   ('JRR Tolken', '1892-01-03', 'Bloemfontein, South Africa');
 
 INSERT INTO reviewer
-(name, company)
+  (name, company)
 VALUES
-('bob', 'Nike'),
-('zeus', 'addidas');
+  ('bob', 'Nike'),
+  ('zeus', 'addidas');
 
-  INSERT INTO review 
-    (rating, reviewer_id, review, book_id)
-    VALUES
-    (5, '1', 'good book', '1'),
-    (3, '2', 'bad book', '2');
+INSERT INTO review 
+  (rating, reviewer_id, review, book_id)
+VALUES
+  (5, '1', 'good book', '1'),
+  (3, '2', 'bad book', '2');
 
 INSERT INTO author_book
 (author_id, book_id)
